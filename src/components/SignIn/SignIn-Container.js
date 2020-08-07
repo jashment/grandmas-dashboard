1ad758a4152e36352a08a74a000792fa5f6cae2f
Jordan <jordanashment@gmail.com>
2020-08-06 22:01:57 -0600

    Starting to add google sign in

---
+++ src/components/SignIn/SignIn-Container.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignIn from './SignIn'
+import * as firebase from 'firebase'
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+const SignInContainer = () => {
+
+}
+
+export default SignInContainer
